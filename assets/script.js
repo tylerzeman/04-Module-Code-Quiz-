bdfc8e9842b3baf290a75130091dfedb53e0ee93
Tyler Zeman <tylerzeman1999@gmai9l.com>
2022-07-05 15:02:21 -0500

    cleaned up/modified code

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,32 @@
+// Variables
+
 var timerBox = document.querySelector('#count');
 
 var timerBoxEl = document.getElementById("count");
 
-var btn = document.querySelector("button");
-
-timerBox.addEventListener('click', startTimer);
+var btn = document.querySelector("#startQuiz");
 
 var counter = 75;
 
-var incorrectAns = document.querySelector('IncorrectAnswer');
+var incorrectAnswer = document.querySelector('.incorrectAnswer');
 
-incorrectAns.addEventListener('onclick', timerBoxEl - 10);
+var mainTimer
 
+// Event Listeners
 
-function startTimer()
+timerBox.addEventListener('click', startTimer);
+
+incorrectAnswer.addEventListener('click', wrongAnswer);
+
+//Functions
+
+function wrongAnswer() {
+
+}
+
+function startTimer() {
 // The timer begins
-{
-  setInterval(function () {
+  mainTimer=setInterval(function () {
     counter--;
     if (counter >= 0) {
       timerBoxEl.textContent = counter;
@@ -27,31 +37,38 @@ function startTimer()
     }
   }, 1000);
 }
-
 function start() {
   timerBoxEl.style = "color:red;";
   btn.setAttribute("style", "display:none");
   startTimer();
   // function to move onto question 1\
-  nextQuestion()
+  getQuestion1()
 
 }
-
-function nextQuestion () {
+function getQuestion1 () {
   var question1 = 0;
   question1++;
   document.querySelector('[data-number="1"]').setAttribute("style", "display:none");
   document.querySelector('[data-number="1"]').setAttribute("style", "display:block");
-
+  // if (correctAnswer = true) {
+  //   alert('Correct!');
+  // }
 }
-
 function getQuestion2 () {
   document.querySelector('[data-number="1"]').setAttribute("style", "display:none");
-  // document.querySelector('[data-number="2"]').setAttribute("style", "display:none");
   document.querySelector('[data-number="2"]').setAttribute("style", "display:block");
-}
+  // if (correctAnswer = true) {
+  //   alert('Correct!');
+  // }
+  }
 function getQuestion3 () {
   document.querySelector('[data-number="2"]').setAttribute("style", "display:none");
-  // document.querySelector('[data-number="3"]').setAttribute("style", "display:none");
   document.querySelector('[data-number="3"]').setAttribute("style", "display:block");
+  // if (correctAnswer = true) {
+  //   alert('Correct!');
+  // }
+}
+function saveHighScore () {
+// Researching local storage and how to save high scores, will resubmit later with working code.
+// Also looking for feedback on anything in this submitted HW - Thank you!
 }
